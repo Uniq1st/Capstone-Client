8fd30b441fd8b93dcb73c3eba4461830a9297e88
Haziq Saddique <haz234@Haziqs-MacBook-Pro.local>
2020-06-17 13:21:46 -0400

    pushing some files still got some work to do now wr=orking on loading data and by working with api and still need to implement more css need help with react

---
+++ src/Component/searchApi.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+class searchApi extends Component{
+    constructor(props) {
+        super(props);
+        this.state = { anime: "", definitions: [] };
+}
+}
+export default searchApi;
